6fdec322b1078e711a31a93d8a0f627b09653529
buyanzhen <16621103082@163.com>
2020-06-04 16:29:05 +0800

    首次commit

--- cloudalibaba-config-nacos-client3377/src/main/java/com/byz/springcloud/ConfigNacosClient3377Main.java
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/byz/springcloud/ConfigNacosClient3377Main.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * nacos支持的三种模式
+ * 1、单机模式，用于测试和单机试用（nacos数据持久化，仅该模式支持，只支持MySQL）
+ * 2、集群模式，用于生产环境，保证高可用
+ * 3、多集群模式，用于多数据中心场景
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ConfigNacosClient3377Main {
